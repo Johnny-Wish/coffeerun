fa527fb0dc0867752dc131738091f7af3884079e
Shuheng Johnny Liu <wish1104@outlook.com>
2020-02-18 16:15:00 +0800

    implement remote data store using ajax

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,16 @@
     'use strict'
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    var SERVER_URL = 'https://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
     var App = window.App;
     var Truck = App.Truck;
+    var RemoteDataStore = App.RemoteDataStore;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var Checklist = App.Checklist;
-    var myTruck = new Truck('ncc-1701', new DataStore());
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var myTruck = new Truck('ncc-1701', remoteDS);
     window.myTruck = myTruck;
     var checklist = new Checklist(CHECKLIST_SELECTOR);
     checklist.addClickListener(myTruck.deliverOrder.bind(myTruck));
